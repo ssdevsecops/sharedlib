8be9a2e5f4a8d82a219497231f5daf1a2af84fc3
Satya Saranu <Satya.Saranu@local.com>
2020-11-12 19:22:15 -0600

    Inital commi

---
+++ vars/ansible.groovy
@@ -0,0 +1,30 @@
+def call(){
+
+    pipeline {
+    agent any
+    
+    stages {
+        stage('ansible_lynt') {
+            steps {
+                sh "ansible-playbook apply_role.yml --syntax-check"
+            }
+        }
+
+        stage('Ansible_Apply') {
+            steps {
+                sshagent(['ec2-user']) {
+                    sh "ansible-playbook apply_role.yml"
+                }
+            }
+        }
+
+        stage('test') {
+            steps {
+                sh "echo ansible run complete"
+            }
+        }
+
+    }
+}
+
+}
